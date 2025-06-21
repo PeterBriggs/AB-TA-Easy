9e3375350834a2311c2434829ecc978f1eef8000
Peter Briggs <peterjamesbriggs@gmail.com>
2025-06-21 15:28:35 +0200

    :sparkles: Accept city quantity and name input

--- ProblemA/Program.cs
+++ ProblemA/Program.cs
@@ -8,6 +8,12 @@
         for (int testCaseIndex = 0; testCaseIndex < numberOfTestCases; testCaseIndex++)
         {
             Console.WriteLine($"Case #{testCaseIndex}:");
+            int numberOfCities = int.Parse(Console.ReadLine() ?? "0");
+            for (int cityIndex = 0; cityIndex < numberOfCities; cityIndex++)
+            {
+                Console.WriteLine($"City #{cityIndex}:");
+                string cityName = Console.ReadLine() ?? "";
+            }
         }
     }
 }
