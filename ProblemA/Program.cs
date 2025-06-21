aa57d9461275a74a92f9412fc18a0428bf3cd258
Peter Briggs <peterjamesbriggs@gmail.com>
2025-06-21 15:37:04 +0200

    :recycle: Separate city processing

--- ProblemA/Program.cs
+++ ProblemA/Program.cs
@@ -5,15 +5,31 @@
         int numberOfTestCases = int.Parse(Console.ReadLine() ?? "0");
 
         Console.WriteLine("Number of cases:" + numberOfTestCases);
+
         for (int testCaseIndex = 0; testCaseIndex < numberOfTestCases; testCaseIndex++)
         {
             Console.WriteLine($"Case #{testCaseIndex}:");
+
             int numberOfCities = int.Parse(Console.ReadLine() ?? "0");
-            for (int cityIndex = 0; cityIndex < numberOfCities; cityIndex++)
-            {
-                Console.WriteLine($"City #{cityIndex}:");
-                string cityName = Console.ReadLine() ?? "";
-            }
+            int uniqueCitiesCount = ProcessCitiesAndGetUniqueCount(numberOfCities);
+
+            Console.WriteLine("Unique cities:");
+            Console.WriteLine(uniqueCitiesCount);
         }
+    }
+
+    static int ProcessCitiesAndGetUniqueCount(int numberOfCities)
+    {
+        HashSet<string> cities = new HashSet<string>();
+
+        for (int cityIndex = 0; cityIndex < numberOfCities; cityIndex++)
+        {
+            Console.WriteLine($"City #{cityIndex}:");
+
+            string city = Console.ReadLine() ?? "";
+            cities.Add(city);
+        }
+
+        return cities.Count;
     }
 }
