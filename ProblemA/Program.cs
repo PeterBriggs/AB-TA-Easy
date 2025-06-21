b5546ca705ee51131010bac53166241182268738
Peter Briggs <peterjamesbriggs@gmail.com>
2025-06-21 15:15:09 +0200

    :test_tube: Read number of test cases

---
+++ ProblemA/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int T = int.Parse(Console.ReadLine() ?? "0");
+
+        Console.WriteLine("Number of cases:" + T);
+        for (int t = 0; t < T; t++)
+        {
+            Console.WriteLine($"Case #{t}:");
+        }
+    }
+}
